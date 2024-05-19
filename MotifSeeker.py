6f0c4e751a9a6d2264321e83e66ea2a4d3459ad7
Justin K Hii <jhii@ucsd.edu>
2024-05-18 17:23:48 -0700

    Initial commit

---
+++ MotifSeeker.py
@@ -0,0 +1,26 @@
+import argparse
+from argparse import RawTextHelpFormatter
+
+
+# For testing, remember to `pip install argparser` in your terminal
+# To run MotifSeeker in terminal, `python MotifSeeker.py -h`
+
+
+
+# Create parser #
+parser = argparse.ArgumentParser(description= 'Welcome to MotifSeeker!\n', formatter_class=RawTextHelpFormatter)
+
+
+
+# Create subparsers #
+
+
+
+
+# Define arguments here #
+# parser.add_argument('-return', type=int, help="Test return an int")
+    
+# Parse arguments here #
+args = parser.parse_args()
+    
+    
