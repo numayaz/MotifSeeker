179e9c4b6a5f665cac41989500a952b45b7a30e0
tkarnani <119444439+tkarnani@users.noreply.github.com>
2024-05-24 14:39:41 -0700

    Create motifseeker.py

---
+++ motifseeker/motifseeker.py
@@ -0,0 +1,59 @@
+# Imports #
+# argparse: Python command line interface
+# numpy, bed_reader: Parse .bed file
+
+import argparse
+from argparse import RawTextHelpFormatter
+import os
+import sys
+import numpy as np
+import math
+import seqlogo
+import scipy
+import random
+from bed_reader import open_bed, sample_file
+
+# To run MotifSeeker in terminal, `python MotifSeeker.py -h`
+# Here's how HOMER performs MEF (http://homer.ucsd.edu/homer/ngs/peakMotifs.html)
+
+# Variables/Objects required
+nucs = {"A": 0, "C": 1, "G": 2, "T": 3}
+
+
+def main(): 
+    # Create parser
+    parser = argparse.ArgumentParser(prog="MotifSeeker", description= "Command-line tool to perform Motif Enrichment Analysis", formatter_class=RawTextHelpFormatter)
+
+    # Input file, we want it to work on .bed files #
+    parser.add_argument("inputfile", help=".bed file containing peaks", type=str)
+
+    parser.add_argument("-r", "--ref", help="fasta reference genome file", \
+                        metavar="FILE", type=str)
+
+    # Other arguments
+    parser.add_argument("-o", "--out", help="Write output to file." \
+                        "Default: stdout", metavar="FILE", type=str, required=False)
+    
+    parser.add_argument("-p", "--pval", help="p value threshold for motif sequences" \
+                         "Default: 0.05", metavar="NUMERICAL P VALUE", type=float, required=False)
+    
+    parser.add_argument("-s", "--size", help="size of motif" \
+                        "Default: 100", metavar="NUMERICAL SIZE VALUE", type=int, required=False)
+
+    # Parse arguments here
+    args = parser.parse_args()
+        
+    # Setup output file
+    if args.out is None:
+        outf = sys.stdout
+    else: 
+        outf = open(args.out, "w")
+
+    # Load and parse BED using bed_reader
+    filename = sample_file(args.inputfile)
+    bed = open_bed(filename)
+    val = bed.read()
+
+    # Test
+    print(val.shape)
+    del bed
