f2ff6dc37ae3a65d08e9313cb7a895502797b19b
jhii7 <justinkylehii@gmail.com>
2024-05-28 20:01:04 -0700

    functions.py update

--- motifseeker/motifseekv0.py
+++ motifseeker/motifseekv0.py
@@ -3,21 +3,20 @@ from argparse import RawTextHelpFormatter
 import os
 import sys
 import numpy as np
+from . import functions
 
 nucs = {"A": 0, "C": 1, "G": 2, "T": 3}
 
-
-#def main(): 
-    # Create parser
+# Create parser
 parser = argparse.ArgumentParser(prog="MotifSeeker", description= "Command-line tool to perform Motif Enrichment Analysis", formatter_class=RawTextHelpFormatter)
 
-    # Input file, we want it to work on .bed files #
+# Input file, we want it to work on .bed files #
 parser.add_argument("inputfile", help=".bed file containing peaks", type=str)
 
 parser.add_argument("-f", "--fasta-ref", help="fasta reference genome file", \
                         metavar="FILE", type=str)
 
-    # Other arguments
+# Other arguments
 parser.add_argument("-o", "--out", help="Write output to file." \
                         "Default: stdout", metavar="FILE", type=str, required=False)
     
@@ -27,10 +26,7 @@ parser.add_argument("-p", "--pval", help="p value threshold for motif sequences"
 parser.add_argument("-s", "--size", help="size of motif" \
                         "Default: 100", metavar="NUMERICAL SIZE VALUE", type=int, required=False)
 
-#parser.add_argument("--version", help="Print the version and quit", \
-#    action="version", version = '{version}'.format(version=__version__))
-
-    # Parse arguments here
+# Parse arguments here
 args = parser.parse_args()
 
 if args.size is not None:
@@ -38,7 +34,7 @@ if args.size is not None:
 else:
         size = 100
         
-    # Setup output file
+# Setup output file
 if args.out is None:
         outf = sys.stdout
 else: 
